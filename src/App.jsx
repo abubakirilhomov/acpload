2b185fbdf8c4bdd71bda04e8d0c07b02005a0ce1
Azamat1855 <azamatshomaksudov7@gmail.com>
2025-01-29 00:44:55 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
+    <main><Outlet/></main>
     <Footer/>
     </>
   )
