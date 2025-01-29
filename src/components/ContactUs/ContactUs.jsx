b9a9535c387d67bf9e670b9b623abe7f167dc4f6
Azamat1855 <azamatshomaksudov7@gmail.com>
2025-01-30 00:23:48 +0500

    ASD

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="w-full max-w-[90%] mx-auto my-10 px-5">
+    <div className="w-full max-w-[90%] mx-auto my-10">
       <h2 className="text-3xl font-semibold text-center mb-8 text-white font-serif">
         {t("contact-us.title")}
       </h2>
